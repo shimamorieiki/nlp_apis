6e0f5177be22c6cbbadced5a80fb5dd36db4f874
shimamorieiki <aifore.2270@gmail.com>
2022-10-23 04:17:05 +0000

    fix janome

--- python/janome/main.py
+++ python/janome/main.py
@@ -2,38 +2,12 @@
 Returns:
     _type_: _description_
 """
-from typing import Dict, List, Optional
+from typing import Dict, List
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-
-from src.service.janome_service import JanomeService
-
-
-class SentenceModel(BaseModel):
-    """_summary_
-        Sentenceクラス
-    Args:
-        BaseModel (_type_): _description_
-    """
-
-    # 解析対象の文
-    sentence: str
-    # 説明
-    description: Optional[str] = None
-
-
-class SentencesModel(BaseModel):
-    """_summary_
-        Sentenceクラス
-    Args:
-        BaseModel (_type_): _description_
-    """
-
-    # 解析対象の文のリスト
-    sentences: List[str]
-    # 説明
-    description: Optional[str] = None
+from src.models.sentence_model import SentenceModel
+from src.models.sentences_model import SentencesModel
+from src.services.janome_service import JanomeService
 
 
 app = FastAPI()
