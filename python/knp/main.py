8ad46ae0d7c68e86dc26df83e95ed14c57dd5b7d
shimamorieiki <aifore.2270@gmail.com>
2022-10-23 04:23:07 +0000

    fix knp

--- python/knp/main.py
+++ python/knp/main.py
@@ -2,39 +2,12 @@
 Returns:
     _type_: _description_
 """
-from typing import Dict, List, Optional
+from typing import Dict, List
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-
+from src.models.sentence_model import SentenceModel
+from src.models.sentences_model import SentencesModel
 from src.services.knp_service import KNPService
-
-
-class SentenceModel(BaseModel):
-    """_summary_
-        Sentenceクラス
-    Args:
-        BaseModel (_type_): _description_
-    """
-
-    # 解析対象の文
-    sentence: str
-    # 説明
-    description: Optional[str] = None
-
-
-class SentencesModel(BaseModel):
-    """_summary_
-        Sentenceクラス
-    Args:
-        BaseModel (_type_): _description_
-    """
-
-    # 解析対象の文のリスト
-    sentences: List[str]
-    # 説明
-    description: Optional[str] = None
-
 
 app = FastAPI()
 
---
+++ python/knp/src/models/sentences_model.py
@@ -0,0 +1,17 @@
+"""_summary_
+"""
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class SentencesModel(BaseModel):
+    """_summary_
+        Sentenceクラス
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    # 解析対象の文のリスト
+    sentences: List[str]
+    # 説明
+    description: Optional[str] = None
